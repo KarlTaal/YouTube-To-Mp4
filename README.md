dcc35df329867211fc93a69434810466954cf47c
KarlTaal <44434743+KarlTaal@users.noreply.github.com>
2020-09-24 13:11:40 +0300

    project

---
+++ README.md
@@ -0,0 +1,2 @@
+# YouTube-To-Mp4
+Python script that reads YouTube links from txt file and downloads them as mp4 files.
---
+++ downloader.py
@@ -0,0 +1,23 @@
+import pytube
+import sys
+import os
+#pip install pytube3
+# peale seda teha muudatusi allalaetud skriptis link:
+# https://stackoverflow.com/questions/62098925/why-my-youtube-video-downloader-only-downloads-some-videos-and-for-other-videos
+
+
+links  = []
+with open("laulud.txt", "r", encoding="UTF-8") as f:
+    for r in f:
+        links.append(r.strip())
+
+print("Starting...")
+for i in range(len(links)):
+    pytube.YouTube(links[i]).streams.filter(only_audio=True).first().download("songs/")
+    progress = round((i+1) / len(links) * 100)
+    sys.stdout.write("Download progress: %d%%   \r" % (progress) )
+    sys.stdout.flush()
+    if i == len(links)-1:
+        print()
+
+print("Done")
